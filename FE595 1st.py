bb2105eb7c74c4f2a4fd6112dbd09f110d2fac27
KanWu10 <54816452+KanWu10@users.noreply.github.com>
2019-09-10 13:02:05 -0400

    Update FE595 1st.py

--- FE595 1st.py
+++ FE595 1st.py
@@ -1,8 +1,11 @@
+##import needed packages
 import matplotlib.pyplot as plt
 import numpy as np
+##define period and define functions
 x = np.arange(0,2*np.pi,0.1)
 y = np.sin(x)
 z = np.cos(x)
+##plot them and add labels
 plt.plot(x,y,x,z)
 plt.xlabel('0 to 2pi')
 plt.ylabel('sin(x) and cos(x)')
